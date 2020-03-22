b221a936339c2c9169032f9625906e2c0eec3de3
Maxim Rebguns <mrmaxguns@gmail.com>
2020-03-21 19:40:07 -0500

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wonderwords_mrmaxguns",
-    version="1.0.2",
+    version="1.0.3",
     author="Maxim Rebguns",
     author_email="mrmaxguns@gmail.com",
     include_package_data=True,
