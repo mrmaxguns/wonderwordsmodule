66d25b7d4bd05fa418097d8ad974d126233887b4
Maxim Rebguns <mrmaxguns@gmail.com>
2020-09-09 16:36:10 -0500

    feat: updated version and setup.py info

--- wonderwords/__init__.py
+++ wonderwords/__init__.py
@@ -1,4 +1,11 @@
 from .random_word import RandomWord
 from .random_sentence import RandomSentence
 
-__version__ = "2.0.0a1"
+__author__ = "Maxim R."
+__copyright__ = "Copyright 2020, Wonderwords"
+__credits__ = ["Maxim R."]
+__license__ = "MIT"
+__version__ = "2.0.0"
+__maintainer__ = "Maxim R."
+__email__ = "mrmaxguns@gmail.com"
+__status__ = "Production"
